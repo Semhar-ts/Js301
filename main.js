52667c546908f53c5646e20da994ac0fa35167e3
Semhar-ts <seatesol@gmail.com>
2022-01-31 20:45:23 +0100

    modified

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Js file imported")
+
+alert("this is a pop up messeage")
+function solution (N){
+
+let bNum = N.toString(2);
+console.log(bNum);
+for (num in bNum){
+console.log(bNum[num]);
+}
+return 0;
+}
+let number = 20;
+console.log(solution(number));
